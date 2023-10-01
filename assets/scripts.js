fa255832aa325db82c1b4d5224e6160096202615
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-10-01 14:44:00 +0200

    Ajout com et try catch

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,28 @@
+// Attend que le document soit prêt
 $(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
+    try {
+        // Sélectionne l'élément HTML avec la classe "gallery" et initialise la galerie
+        const $gallery = $('.gallery');
+        
+        // Configuration de la galerie
+        let galleryOptions = {
+            columns: {
+                xs: 1,
+                sm: 2,
+                md: 3,
+                lg: 3,
+                xl: 3
+            },
+            lightBox: true, // Active la lightbox //
+            lightboxId: 'myAwesomeLightbox', // ID de la lightbox //
+            showTags: true, // Affiche les tags //
+            tagsPosition: 'top' // Position des tags //
+        };
+        
+        // Initialise la galerie avec les options //
+        $gallery.mauGallery(galleryOptions);
+    } catch (error) {
+        // Gestion des erreurs //
+        console.error('Une erreur s\'est produite :', error);
+    }
 });
