760505d7738ac49cbda7760fe4efb67dfbf242cc
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-10-14 14:37:33 +0200

    Ajout format photo

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -1,47 +1,92 @@
 const dataPictures = {
     "gallery-picture": [
         {
-            "filename": "./images/gallery/concerts/aaron-paul-large.webp",
+            "filename": [
+                "./images/gallery/concerts/aaron-paul-large.webp",
+                "./images/gallery/concerts/aaron-paul-medium.webp",
+                "./images/gallery/concerts/aaron-paul-mini.webp",
+                "./images/gallery/concerts/aaron-paul-375.webp"
+            ],
             "tags": ["concert", "tous"],
             "description": "Concert vu du public"
         },
         {
-            "filename": "./images/gallery/entreprise/ali-morshedlou-large.webp",
+            "filename": [
+                "./images/gallery/entreprise/ali-morshedlou-large.webp",
+                "./images/gallery/entreprise/ali-morshedlou-medium.webp",
+                "./images/gallery/entreprise/ali-morshedlou-mini.webp",
+                "./images/gallery/entreprise/ali-morshedlou-375.webp"
+            ],
             "tags": ["entreprises", "tous"],
             "description": "Homme en costume noir posé"
         },
         {
-            "filename": "./images/gallery/entreprise/jason-goodman-large.webp",
+            "filename": [
+                "./images/gallery/entreprise/jason-goodman-large.webp",
+                "./images/gallery/entreprise/jason-goodman-medium.webp",
+                "./images/gallery/entreprise/jason-goodman-mini.webp",
+                "./images/gallery/entreprise/jason-goodman-375.webp"
+            ],
             "tags": ["entreprises", "tous"],
             "description": "Femme souriante assise au travail"
         },
         {
-            "filename": "./images/gallery/mariage/hannah-busing-large.webp",
+            "filename": [
+                "./images/gallery/mariage/hannah-busing-large.webp",
+                "./images/gallery/mariage/hannah-busing-medium.webp",
+                "./images/gallery/mariage/hannah-busing-mini.webp",
+                "./images/gallery/mariage/hannah-busing-375.webp"
+            ],
             "tags": ["mariages", "tous"],
             "description": "Un homme et une femme se tendant les mains"
         },
         {
-            "filename": "./images/gallery/portraits/ade-tunji-large.webp",
+            "filename": [
+                "./images/gallery/portraits/ade-tunji-large.webp",
+                "./images/gallery/portraits/ade-tunji-medium.webp",
+                "./images/gallery/portraits/ade-tunji-mini.webp",
+                "./images/gallery/portraits/ade-tunji-375.webp"
+            ],
             "tags": ["portrait", "tous"],
             "description": "Portrait d'un homme fermant les yeux"
         },
         {
-            "filename": "./images/gallery/mariage/jakob-owens-large.webp",
+            "filename": [
+                "./images/gallery/mariage/jakob-owens-large.webp",
+                "./images/gallery/mariage/jakob-owens-medium.webp",
+                "./images/gallery/mariage/jakob-owens-mini.webp",
+                "./images/gallery/mariage/jakob-owens-375.webp"
+            ],
             "tags": ["mariages", "tous"],
             "description": "Mariés se tenant la main en extérieur et heureux"
         },
         {
-            "filename": "./images/gallery/portraits/van-prattenburg-large.webp",
+            "filename": [
+                "./images/gallery/portraits/van-prattenburg-large.webp",
+                "./images/gallery/portraits/van-prattenburg-medium.webp",
+                "./images/gallery/portraits/van-prattenburg-mini.webp",
+                "./images/gallery/portraits/van-prattenburg-375.webp"
+            ],
             "tags": ["portrait", "tous"],
             "description": "Portrait d'une jeune femme blanche à lunettes"
         },
         {
-            "filename": "./images/gallery/concerts/austin-neill-large.webp",
+            "filename": [
+                "./images/gallery/concerts/austin-neill-large.webp",
+                "./images/gallery/concerts/austin-neill-medium.webp",
+                "./images/gallery/concerts/austin-neill-mini.webp",
+                "./images/gallery/concerts/austin-neill-375.webp"
+            ],
             "tags": ["concert", "tous"],
             "description": "Portrait d'un chanteur vu de la foule"
         },
         {
-            "filename": "./images/gallery/entreprise/mateus-campos-felipe-large.webp",
+            "filename": [
+                "./images/gallery/entreprise/mateus-campos-felipe-large.webp",
+                "./images/gallery/entreprise/mateus-campos-felipe-medium.webp",
+                "./images/gallery/entreprise/mateus-campos-felipe-mini.webp",
+                "./images/gallery/entreprise/mateus-campos-felipe-375.webp"
+            ],
             "tags": ["entreprises", "tous"],
             "description": "Femme souriante assise à son bureau"
         }
@@ -59,29 +104,29 @@ document.addEventListener("DOMContentLoaded", function () {
                 picture.className = "picture-item";
 
                 const sourceLarge = document.createElement("source");
-                sourceLarge.srcset = item.filename;
+                sourceLarge.srcset = item.filename.join(', ');
                 sourceLarge.media = "(min-width: 992px)";
                 sourceLarge.type = "image/webp";
 
                 const sourceMedium = document.createElement("source");
-                sourceMedium.srcset = item.filename.replace("-large.webp", "-medium.webp");
+                sourceMedium.srcset = item.filename.join(', ').replace("-large.webp", "-medium.webp");
                 sourceMedium.media = "(min-width: 576px)";
                 sourceMedium.type = "image/webp";
 
                 const sourceMini = document.createElement("source");
-                sourceMini.srcset = item.filename.replace("-medium.webp", "-mini.webp");
+                sourceMini.srcset = item.filename.join(', ').replace("-medium.webp", "-mini.webp");
                 sourceMini.media = "(min-width: 376px)";
                 sourceMini.type = "image/webp";
 
                 const source375 = document.createElement("source");
-                source375.srcset = item.filename.replace("-mini-webp", "-375.webp");
+                source375.srcset = item.filename.join(', ').replace("-mini.webp", "-375.webp");
                 source375.media = "(max-width: 375px)";
                 source375.type = "image/webp";
 
                 const img = document.createElement("img");
                 img.className = "gallery-picture";
                 img.dataset.galleryTag = item.tags[0];
-                img.src = item.filename.replace("-large.webp", "-mini.webp");
+                img.src = item.filename.join(', ').replace("-large.webp", "-mini.webp");
                 img.alt = item.description;
 
                 window.addEventListener("resize", function() {
